c5e31546ac16a9243f4b9158b7e778a31dfa1391
BrezenFan <ozan.mgl@gmail.com>
2025-01-09 12:03:57 +0100

    Adds barebones window

---
+++ GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+# Ein Fenster erstellen
+fenster = Tk()
+# Den Fenstertitel erstellen
+fenster.title("ShortCutLib")
+
+#In der Ereignisschleife auf Eingabe des Benutzers warten.
+fenster.mainloop()
