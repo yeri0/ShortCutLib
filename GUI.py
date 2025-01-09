9b6a874dd6a4da1fdb949e46d5d08aa5920c3665
BrezenFan <ozan.mgl@gmail.com>
2025-01-09 13:23:23 +0100

    Adds Popupwindow

--- GUI.py
+++ GUI.py
@@ -1,9 +1,43 @@
 from tkinter import *
+import tkinter as tk
+from tkinter import simpledialog
+
+#Funktion soll ein Dialogfenster öffnen, dass dann mit den nötigen Infos einen neuen Shortcut zur Lib hinzufügt
+def add_button_action():
+    print("add_button_action was clicked")
+    #Dialog Fenster initialisieren
+    hinzufuegen_pop_up = Toplevel(fenster)
+    hinzufuegen_pop_up.geometry("20x300")
+    hinzufuegen_pop_up.title("Shortcut hinzufuegen")
+    shortcut = Entry(hinzufuegen_pop_up, bd=5, width=40)
+    shortcut_label = Label(hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
+    describtion = Entry(hinzufuegen_pop_up, bd=5, width=40)
+    describtion_label=Label(hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
+    shortcut_label.pack()
+    shortcut.pack()
+    describtion_label.pack()
+    describtion.pack()
+    print(shortcut)
+    print(describtion)
+
+#Funktion soll Dialogfenster öffnen, dass bestimmten Shortcut wieder entfernt
+def delete_button_action():
+    print("delete_button_action was pressed")
 
 # Ein Fenster erstellen
 fenster = Tk()
+#Geometrie einstellen
+fenster.geometry("750x480")
 # Den Fenstertitel erstellen
 fenster.title("ShortCutLib")
+
+#Add Button erstellen
+add_button = Button(fenster, text="Hinzufügen", command = add_button_action)
+delete_button = Button(fenster, text="Entfernen", command = delete_button_action)
+
+#Button zum Fenster hinzufügen
+add_button.grid(row = 0, column = 0, sticky="e", padx=5, pady=5)
+delete_button.grid(row = 0, column = 1, sticky="e", padx=5, pady=5)
 
 #In der Ereignisschleife auf Eingabe des Benutzers warten.
 fenster.mainloop()
