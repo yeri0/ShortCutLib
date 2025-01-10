677ca073c5f40e141b11ebc0283255f6ac3e8ef2
BrezenFan <ozan.mgl@gmail.com>
2025-01-10 10:32:01 +0100

    Auslagerung des Popups in eine Klasse

--- GUI.py
+++ GUI.py
@@ -1,36 +1,14 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import simpledialog
-
-def read_entry_data(shortcut_entry, describtion_entry):
-    shortcut = shortcut_entry.get()
-    describtion = describtion_entry.get()
-    print(shortcut)
-    print(describtion)
+from system import *
+import Popup
 
 #Funktion soll ein Dialogfenster öffnen, dass dann mit den nötigen Infos einen neuen Shortcut zur Lib hinzufügt
 def add_button_action():
     print("add_button_action was clicked")
-
-    #Dialog Fenster initialisieren
-    hinzufuegen_pop_up = Toplevel(fenster)
-    hinzufuegen_pop_up.geometry("400x200")
-    hinzufuegen_pop_up.title("Shortcut hinzufuegen")
-
-    #Eingabefelder erstellen
-    shortcut_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
-    shortcut_label = Label(hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
-    describtion_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
-    describtion_label=Label(hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
-
-    #Submit button erstellen
-    submit_button = Button(hinzufuegen_pop_up, text = "Erstellen", command = read_entry_data(shortcut_entry, describtion_entry))
-
-    #Elemente anzeigen:
-    shortcut_label.pack()
-    shortcut.pack()
-    describtion_label.pack()
-    describtion.pack()
+    popup = Popup.Popup(fenster)
+    
 
 
 #Funktion soll Dialogfenster öffnen, dass bestimmten Shortcut wieder entfernt
---
+++ Popup.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import simpledialog
+from system import *
+
+class Popup:
+    def __init__(self, root):
+        #Dialog Fenster initialisieren
+        hinzufuegen_pop_up = Toplevel(root)
+        hinzufuegen_pop_up.geometry("400x200")
+        hinzufuegen_pop_up.title("Shortcut hinzufuegen")
+        #Eingabefelder erstellen
+        self.shortcut_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
+        shortcut_label = Label(hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
+        self.description_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
+        description_label=Label(hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
+
+        #Submit button erstellen
+        submit_button = Button(hinzufuegen_pop_up, text = "Erstellen", command = self.read_entry_data)
+
+        #Elemente anzeigen:
+        shortcut_label.pack()
+        self.shortcut_entry.pack()
+        description_label.pack()
+        self.description_entry.pack()
+        submit_button.pack()
+        print("The Popup has been created")
+    
+    def read_entry_data(self):
+        print("read_entry_data")
+        shortcut = self.shortcut_entry.get()
+        description = self.description_entry.get()
+        print(shortcut)
+        print(description)
