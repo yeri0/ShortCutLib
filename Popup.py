1f31dd9f92ee5e063b2a99625b71d56eda0daa81
BrezenFan <ozan.mgl@gmail.com>
2025-01-10 18:06:03 +0100

    closes the popup Window after the action of adding a Shortcut is complete

--- Popup.py
+++ Popup.py
@@ -5,17 +5,17 @@ from databank import *
 class Popup:
     def __init__(self, root):
         #Dialog Fenster initialisieren
-        hinzufuegen_pop_up = Toplevel(root)
-        hinzufuegen_pop_up.geometry("400x200")
-        hinzufuegen_pop_up.title("Shortcut hinzufuegen")
+        self.hinzufuegen_pop_up = Toplevel(root)
+        self.hinzufuegen_pop_up.geometry("400x200")
+        self.hinzufuegen_pop_up.title("Shortcut hinzufuegen")
         #Eingabefelder erstellen
-        self.shortcut_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
-        shortcut_label = Label(hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
-        self.description_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
-        description_label=Label(hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
+        self.shortcut_entry = Entry(self.hinzufuegen_pop_up, bd=5, width=40)
+        shortcut_label = Label(self.hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
+        self.description_entry = Entry(self.hinzufuegen_pop_up, bd=5, width=40)
+        description_label=Label(self.hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
 
         #Submit button erstellen
-        submit_button = Button(hinzufuegen_pop_up, text = "Erstellen", command = self.read_entry_data)
+        submit_button = Button(self.hinzufuegen_pop_up, text = "Erstellen", command = self.read_entry_data)
 
         #Elemente anzeigen:
         shortcut_label.pack()
@@ -30,3 +30,4 @@ class Popup:
         shortcut = self.shortcut_entry.get()
         description = self.description_entry.get()
         add_shortcut(Shortcut("",shortcut,description,))
+        self.hinzufuegen_pop_up.destroy()
