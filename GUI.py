3ff55aac0077f972883e97f71f2543ae9888b615
BrezenFan <ozan.mgl@gmail.com>
2025-01-10 09:56:24 +0100

    adds submit button

--- GUI.py
+++ GUI.py
@@ -2,23 +2,36 @@ from tkinter import *
 import tkinter as tk
 from tkinter import simpledialog
 
+def read_entry_data(shortcut_entry, describtion_entry):
+    shortcut = shortcut_entry.get()
+    describtion = describtion_entry.get()
+    print(shortcut)
+    print(describtion)
+
 #Funktion soll ein Dialogfenster öffnen, dass dann mit den nötigen Infos einen neuen Shortcut zur Lib hinzufügt
 def add_button_action():
     print("add_button_action was clicked")
+
     #Dialog Fenster initialisieren
     hinzufuegen_pop_up = Toplevel(fenster)
-    hinzufuegen_pop_up.geometry("20x300")
+    hinzufuegen_pop_up.geometry("400x200")
     hinzufuegen_pop_up.title("Shortcut hinzufuegen")
-    shortcut = Entry(hinzufuegen_pop_up, bd=5, width=40)
+
+    #Eingabefelder erstellen
+    shortcut_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
     shortcut_label = Label(hinzufuegen_pop_up, text="Geben Sie Ihren Shortcut ein:")
-    describtion = Entry(hinzufuegen_pop_up, bd=5, width=40)
+    describtion_entry = Entry(hinzufuegen_pop_up, bd=5, width=40)
     describtion_label=Label(hinzufuegen_pop_up, text="Beschreiben Sie Ihr Shortcut:")
+
+    #Submit button erstellen
+    submit_button = Button(hinzufuegen_pop_up, text = "Erstellen", command = read_entry_data(shortcut_entry, describtion_entry))
+
+    #Elemente anzeigen:
     shortcut_label.pack()
     shortcut.pack()
     describtion_label.pack()
     describtion.pack()
-    print(shortcut)
-    print(describtion)
+
 
 #Funktion soll Dialogfenster öffnen, dass bestimmten Shortcut wieder entfernt
 def delete_button_action():
